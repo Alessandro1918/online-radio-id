aa12b31ed10a39a3147e1c573ca750904ef39cb5
Alessandro1918 <alessandro1918@hotmail.com>
2026-03-05 15:06:44 -0300

    style: add basic loading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { RadioList } from "./components/radio-list"
 export default function Home() {
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <RadioList />
     </div>
   )
