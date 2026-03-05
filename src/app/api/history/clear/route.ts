641236fe9cd1b44ddbd68d6bf7d69b03e46e44b8
Alessandro1918 <alessandro1918@hotmail.com>
2026-03-05 16:15:31 -0300

    feat: new route: delete old db records

---
+++ src/app/api/history/clear/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from "next/server"
+import { db } from "../../../../db/connection"
+import { schema } from "../../../../db/schema/index"
+import { lt } from "drizzle-orm"
+import dayjs from "dayjs"
+
+// Delete all the db records older than "n" days
+// http://localhost:3000/api/history/clear
+export async function DELETE(req: NextRequest) {
+
+  const today = dayjs()
+
+  const MAX_PAST_DAYS = 7     // enable history to go back from today + "n" past days
+
+  await db
+    .delete(schema.ids)
+    .where(lt(schema.ids.timestamp, today.subtract(MAX_PAST_DAYS + 1, "day").toDate()))
+  return new Response(null, {status: 204})
+}
